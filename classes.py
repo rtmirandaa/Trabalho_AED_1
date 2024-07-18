f07d984f7694fb859567ee622ac2313680a27ed0
rtmiranda <rtmiranda008@gmail.com>
2024-07-18 16:21:28 -0300

    Trabalho Pronto

--- classes.py
+++ classes.py
@@ -1,209 +1,73 @@
-# Define a classe Torre
+proximo_id_apartamento = 0
+
+def gerar_proximo_id():
+    global proximo_id_apartamento
+    id_atual = proximo_id_apartamento
+    proximo_id_apartamento += 1
+    return id_atual
+
 class Torre:
-    # Inicializa a classe Torre com nome e endereco
-    def __init__(self, nome, endereco):
-        self.id = id  # Atribui um identificador à torre
-        self.nome = nome  # Atribui o nome à torre
-        self.endereco = endereco  # Atribui o endereco à torre
+    def __init__(self, id, nome, endereco):
+        self.id = id
+        self.nome = nome
+        self.endereco = endereco
 
-    # Define a representação em string da classe Torre
-    def __str__(self):
-        return f"Nome: {self.nome} \n Endereço:{self.endereco} \n"
+class Apartamento:
+    def __init__(self, id, numero_apartamento, numero_vaga, torre):
+        self.id = id
+        self.numero_apartamento = numero_apartamento
+        self.numero_vaga = numero_vaga
+        self.torre = torre
 
-    # Método para cadastrar a torre (ainda não implementado)
-    def cadastrar(self):
-        pass
-
-    # Método para imprimir a representação da torre
-    def imprimir(self):
-        return str(self)
-
-# Define a classe apartamento
-class apartamento:
-    # Inicializa a classe apartamento com um numero
-    def __init__(self, numero):
-        self.id = 0  # Atribui um identificador ao apartamento
-        self.numero = numero  # Atribui o número ao apartamento
-        self.torre = None  # Inicializa a torre como None
-        self.vaga = int  # Inicializa a vaga como tipo inteiro
-
-    # Método para cadastrar um apartamento em uma torre
-    def cadastrar(self, torre):
-        self.torre = torre  # Atribui a torre ao apartamento
-        print("Cadastrado com sucesso")
-        return
-
-    # Define a representação em string da classe apartamento
-    def __str__(self):
-        vaga = ""  # Inicializa a string vaga
-        if self.vaga > 10:  # Verifica se a vaga é maior que 10
-            vaga = "Este apartamento está na fila"  # Atribui uma mensagem à vaga
-        else:
-            vaga = self.vaga  # Atribui o valor da vaga
-        return f"Número: {self.numero} \n Torre: {str(self.torre)} \n Vaga: {self.vaga} \n"
-
-    # Método para imprimir a representação do apartamento
-    def imprimir(self):
-        return str(self)
-
-# Define a classe Fila
-class Fila:
-    # Define a classe nodo para os nós da fila
-    class nodo:
-        # Inicializa a classe nodo com um numero
-        def __init__(self, numero):
-            self.numero = numero  # Atribui o número ao nodo
-            self.proximo = None  # Inicializa o próximo nodo como None
-
-    # Inicializa a classe Fila
+class FilaDeEspera:
     def __init__(self):
-        self.inicio = None  # Inicializa o início da fila como None
-        self.fim = None  # Inicializa o fim da fila como None
-        self.tamanho = 0  # Inicializa o tamanho da fila como 0
+        self.fila = []
 
-    # Define o iterador para a classe Fila
-    def __iter__(self):
-        atual = self.inicio  # Define o nodo atual como o início da fila
-        while atual:  # Enquanto houver um nodo atual
-            yield atual  # Retorna o nodo atual
-            atual = atual.proximo  # Avança para o próximo nodo
+    def adicionar_apartamento(self, apartamento):
+        self.fila.append(apartamento)
+        print(f" {apartamento.numero_apartamento} adicionado à fila de espera.")
 
-    # Define a representação em string da classe Fila
-    def __str__(self):
-        return "\n".join([str(i) for i in self] + "\n")
-
-    # Define o método len para a classe Fila
-    def __len__(self):
-        return self.tamanho
-
-    # Método para inserir um novo nodo na fila
-    def inserir(self, garagem):
-        novo = self.nodo(garagem)  # Cria um novo nodo com o número da garagem
-        if self.fim is None:  # Se o fim da fila é None
-            self.fim = novo  # Define o novo nodo como o fim da fila
+    def retirar_apartamento(self, numero_vaga):
+        if self.fila:
+            apartamento = self.fila.pop(0)
+            apartamento.numero_vaga = numero_vaga
+            print(f"Apartamento {apartamento.numero_apartamento} retirado da fila de espera e atribuído à vaga {numero_vaga}.")
+            return apartamento
         else:
-            self.fim.proximo = novo  # Define o próximo do fim da fila como o novo nodo
-            self.ultimo = novo  # Atualiza o fim da fila para o novo nodo
-        if self.inicio is None:  # Se o início da fila é None
-            self.inicio = novo  # Define o novo nodo como o início da fila
-        self.tamanho += 1  # Incrementa o tamanho da fila
+            print("A fila de espera está vazia.")
+            return None
 
-    # Método para remover um nodo da fila
-    def remover(self):
-        if len(self) is not None:  # Se o tamanho da fila não é None
-            garagem = self.inicio.numero  # Armazena o número do início da fila
-            self.inicio = self.inicio.proximo  # Define o início da fila como o próximo nodo
-            self.tamanho -= 1  # Decrementa o tamanho da fila
-            return garagem  # Retorna o número do nodo removido
-            raise IndexError("Fila vazia")  # Levanta um erro se a fila estiver vazia
+    def imprimir_fila(self):
+        if self.fila:
+            print("Fila de espera:")
+            for apto in self.fila:
+                print(f"Apartamento {apto.numero_apartamento}, {apto.torre.nome}")
+        else:
+            print("A fila de espera está vazia.")
 
-# Define a classe lista
-class lista:
-    # Define a classe nodo para os nós da lista
-    class nodo:
-        # Inicializa a classe nodo com um valor
-        def __init__(self, valor):
-            self.valor = valor  # Atribui o valor ao nodo
-            self.proximo = None  # Inicializa o próximo nodo como None
-
-        # Define a representação em string da classe nodo
-        def __str__(self):
-            return str(self.valor)
-
-    # Inicializa a classe lista
+class ListaApartamentosComVaga:
     def __init__(self):
-        self.inicio = None  # Inicializa o início da lista como None
-        self.fim = None  # Inicializa o fim da lista como None
-        self.tamanho = 0  # Inicializa o tamanho da lista como 0
+        self.apartamentos = []
 
-    # Define o método getitem para acessar itens na lista
-    def __getitem__(self, posicao):
-        if isinstance(posicao, slice):  # Verifica se a posição é um slice
-            passo = posicao.step if posicao.step is not None else 1  # Define o passo do slice
-            if passo == 0:
-                raise ValueError("Passo não pode ser zero.")  # Levanta um erro se o passo for zero
-            if passo > 0:
-                inicio = posicao.start if posicao.start is not None else 0  # Define o início do slice
-                fim = posicao.stop if posicao.stop is not None else len(self)  # Define o fim do slice
-            else:
-                inicio = posicao.start if posicao.start is not None else len(self) - 1  # Define o início do slice
-                fim = posicao.stop if posicao.stop is not None else -1  # Define o fim do slice
-        if posicao < 0:
-            posicao = len(self) + posicao  # Ajusta a posição se for negativa
-        if posicao < 0 or posicao >= self.tamanho:
-            raise IndexError("Posicao inválida")  # Levanta um erro se a posição for inválida
-        atual = self.inicio  # Define o nodo atual como o início da lista
-        for i in range(posicao):
-            atual = atual.proximo  # Avança para o próximo nodo
-        return atual  # Retorna o nodo na posição especificada
+    def adicionar_apartamento(self, apartamento):
+        self.apartamentos.append(apartamento)
+        self.apartamentos.sort(key=lambda apto: apto.numero_vaga)
+        print(f"Apartamento {apartamento.numero_apartamento} com vaga {apartamento.numero_vaga} adicionado à lista de apartamentos com vaga.")
 
-    # Define o iterador para a classe lista
-    def __iter__(self):
-        atual = self.inicio  # Define o nodo atual como o início da lista
-        while atual is not None:  # Enquanto houver um nodo atual
-            yield atual  # Retorna o nodo atual
-            atual = atual.proximo  # Avança para o próximo nodo
+    def liberar_vaga(self, numero_vaga, fila_de_espera):
+        for apto in self.apartamentos:
+            if apto.numero_vaga == numero_vaga:
+                self.apartamentos.remove(apto)
+                print(f"Vaga {numero_vaga} liberada pelo Apartamento {apto.numero_apartamento}.")
+                fila_de_espera.adicionar_apartamento(apto)
+                return numero_vaga
+        print(f"Vaga {numero_vaga} não encontrada.")
+        return None
 
-    # Define o método delitem para remover itens da lista
-    def __delitem__(self, posicao):
-        if isinstance(posicao, slice):  # Verifica se a posição é um slice
-            passo = posicao.step if posicao.step is not None else 1  # Define o passo do slice
-            if passo == 0:
-                raise ValueError("Passo não pode ser zero.")  # Levanta um erro se o passo for zero
-            if passo > 0:
-                inicio = posicao.start if posicao.start is not None else 0  # Define o início do slice
-                fim = posicao.stop if posicao.stop is not None else len(self)  # Define o fim do slice
-            else:
-                inicio = posicao.start if posicao.start is not None else len(self) - 1  # Define o início do slice
-                fim = posicao.stop if posicao.stop is not None else -1  # Define o fim do slice
-        if posicao < 0:
-            posicao = len(self) + posicao  # Ajusta a posição se for negativa
-        if posicao < 0 or posicao >= self.tamanho:
-            raise IndexError("Posicao inválida")  # Levanta um erro se a posição for inválida
-        atual = self.inicio  # Define o nodo atual como o início da lista
-        for i in range(posicao):
-            anterior = atual  # Armazena o nodo anterior
-            atual = atual.proximo  # Avança para o próximo nodo
-        anterior.proximo = atual.proximo  # Remove o nodo na posição especificada
-        self.tamanho -= 1  # Decrementa o tamanho da lista
-
-    # Define a representação em string da classe lista
-    def __str__(self):
-        return "\n".join([str(valor) for valor in self]) + "\n"
-
-    # Define o método len para a classe lista
-    def __len__(self):
-        return self.tamanho
-
-    # Método para inserir um nodo no final da lista
-    def inserir_final(self, valor):
-        novo = self.nodo(valor)  # Cria um novo nodo com o valor especificado
-        self.tamanho += 1  # Incrementa o tamanho da lista
-        if self.inicio is None:  # Se o início da lista é None
-            self.inicio = novo  # Define o novo nodo como o início da lista
-            self.fim = novo  # Define o novo nodo como o fim da lista
-            return
-        self.fim.proximo = novo  # Define o próximo do fim da lista como o novo nodo
-        self.fim = novo  # Atualiza o fim da lista para o novo nodo
-
-    # Método para inserir um nodo em uma posição específica na lista
-    def inserir(self, posicao, valor):
-        novo = self.nodo(valor)  # Cria um novo nodo com o valor especificado
-        self.tamanho += 1  # Incrementa o tamanho da lista
-        if self.inicio is None:  # Se o início da lista é None
-            self.inicio = novo  # Define o novo nodo como o início da lista
-            self.fim = novo  # Define o novo nodo como o fim da lista
-            return
-        if posicao <= 0:  # Se a posição é menor ou igual a 0
-            novo.proximo = self.inicio  # Define o próximo do novo nodo como o início da lista
-            self.inicio = novo  # Define o novo nodo como o início da lista
-            return
-        x = 0  # Inicializa o contador x
-        atual = self.inicio  # Define o nodo atual como o início da lista
-        while atual.proximo is not None and x < posicao - 1:
-            atual = atual.proximo  # Avança para o próximo nodo
-            x += 1  # Incrementa o contador x
-        if atual.proximo is None:  # Se o próximo nodo é None
-            self.fim = novo  # Define o novo nodo como o fim da lista
-        novo.proximo = atual.proximo  # Define o próximo do novo nodo como o próximo do nodo atual
-        atual.proximo = novo  # Define o próximo do nodo atual como o novo nodo
+    def imprimir_lista(self):
+        if self.apartamentos:
+            print("Apartamentos com vaga:")
+            for apto in self.apartamentos:
+                print(f"Apartamento {apto.numero_apartamento}, {apto.torre.nome}, Vaga {apto.numero_vaga}")
+        else:
+            print("Nenhum apartamento com vaga disponível.")
--- main.py
+++ main.py
@@ -1,85 +1,82 @@
-# Importa todas as classes do módulo classes
-from classes import *
-lista_AP = lista()  # Cria uma instância da classe lista
-fila_Espera = Fila()  # Cria uma instância da classe Fila
+from classes import Torre, Apartamento, FilaDeEspera, ListaApartamentosComVaga, gerar_proximo_id
 
-# Define o menu do sistema
-Menu = """
-----------------------------------------
-Sistema de Gerenciamento de condomínios
-----------------------------------------
-0 - Sair
-1 - Cadastrar Torre
-2 - Cadastrar apartamento
-3 - Visualizar apartamentos com vaga de garagem
-4 - Visualizar fila de espera
-5 - Remover apartamento
-"""
-
-# Função para cadastrar uma torre
-def cadastrar_torre():
-    if len(lista_AP) == 0:  # Verifica se a lista de apartamentos está vazia
-        print("Não podem ser cadastrados Torres sem apartamentos existentes")
-        input("Clique Enter para continuar")
-    else:
-        nome = input("Nome da torre: ")  # Solicita o nome da torre
-        endereco = input("Endereço da torre: ")  # Solicita o endereço da torre
-        torre = Torre(nome, endereco)  # Cria uma nova instância da classe Torre
-        if len(lista_AP) != 10:  # Verifica se a lista de apartamentos não está cheia
-            print(str(lista_AP))  # Imprime a lista de apartamentos
-            num_ap = int(input("Digite o indice do apartamento que você deseja nesta Torre: "))  # Solicita o índice do apartamento
-            ap = lista_AP[num_ap]  # Obtém o apartamento na posição especificada
-            ap.cadastrar(torre)  # Cadastra o apartamento na torre
-        else:
-            print(str(fila_Espera))  # Imprime a fila de espera
-            num_ap = input("Número do apartamento nesta Torre: ")  # Solicita o número do apartamento
-            ap = fila_Espera[num_ap]  # Obtém o apartamento na posição especificada
-            ap.cadastrar(torre)  # Cadastra o apartamento na torre
-
-# Função para cadastrar um apartamento
-def cadastrar_ap():
-    num_ap = int(input("Número do apartamento: "))  # Solicita o número do apartamento
-    ap = apartamento(num_ap)  # Cria uma nova instância da classe apartamento
-    if len(lista_AP) <= 10:  # Verifica se a lista de apartamentos não está cheia
-        garagem = 0  # Inicializa o contador de garagem
-        garagem += len(lista_AP) + 1  # Incrementa o contador de garagem
-        ap.vaga = garagem  # Atribui a vaga ao apartamento
-        lista_AP.inserir_final(ap)  # Insere o apartamento no final da lista
-        print("Apartamento Cadastrado com sucesso")
-    else:
-        fila_Espera.inserir(ap)  # Insere o apartamento na fila de espera
-
-# Função para exibir o menu do sistema
 def menu():
-    while True:  # Loop infinito para exibir o menu
-        print(Menu)  # Imprime o menu
-        op = int(input("Opção: "))  # Solicita a opção do usuário
-        if op == 0:
-            print("Saindo...")
-            break  # Sai do loop
-        elif op == 1:
-            cadastrar_torre()  # Chama a função para cadastrar uma torre
-        elif op == 2:
-            cadastrar_ap()  # Chama a função para cadastrar um apartamento
-        elif op == 3:
-            print(str(lista_AP))  # Imprime a lista de apartamentos
-            input("Clique Enter para continuar")
-        elif op == 4:
-            print(str(fila_Espera))  # Imprime a fila de espera
-            input("Clique Enter para continuar")
-        elif op == 5:
-            excluir = int(input("Digite 1 para remover na fila de espera ou 2 para remover na lista de apartamentos: "))  # Solicita a opção de exclusão
-            if excluir == 1:
-                print("Será removido o primeiro da fila de espera")
-                print(str(fila_Espera))  # Imprime a fila de espera
-                fila_Espera.remover()  # Remove o primeiro da fila de espera
-            elif excluir == 2:
-                print(str(lista_AP))  # Imprime a lista de apartamentos
-                num_ap = input("Digite o índice do apartamento que deseja excluir: ")  # Solicita o índice do apartamento
-                del lista_AP[num_ap]  # Remove o apartamento na posição especificada
-            else:
-                print("Opcão inválida")  # Imprime uma mensagem de opção inválida
+    fila_de_espera = FilaDeEspera()
+    lista_apartamentos = ListaApartamentosComVaga()
+    vagas_disponiveis = [i for i in range(1, 11)]
 
-# Chama a função menu se o script for executado diretamente
+    torres = [
+        Torre(id=1, nome="Torre: A", endereco="Rua 1, Nº 100"),
+        Torre(id=2, nome="Torre: B", endereco="Rua 2, Nº 200"),
+        Torre(id=3, nome="Torre: C", endereco="Rua 3, Nº 300"),
+        Torre(id=4, nome="Torre: D", endereco="Rua 4, Nº 400")
+    ]
+
+    # for i in range(11):
+    #     numero_apartamento = f" {i+1}"
+    #     numero_vaga = None
+    #     if vagas_disponiveis:
+    #         numero_vaga = vagas_disponiveis.pop(0)
+    #     torre = torres[i % len(torres)]
+    #     id_apartamento = gerar_proximo_id()
+    #     apartamento = Apartamento(id_apartamento, numero_apartamento, numero_vaga, torre)
+
+    #     if numero_vaga is not None:
+    #         lista_apartamentos.adicionar_apartamento(apartamento)
+    #     else:
+    #         fila_de_espera.adicionar_apartamento(apartamento)
+
+    while True:
+        print("\nMenu:")
+        print("1. Cadastrar apartamento")
+        print("2. Liberar vaga")
+        print("3. Imprimir lista de apartamentos com vaga")
+        print("4. Imprimir fila de espera")
+        print("5. Sair")
+
+        opcao = int(input("Escolha uma opção: "))
+
+        if opcao == 1:
+            numero_apartamento = input("Número do apartamento: ")
+            numero_vaga = None
+            if vagas_disponiveis:
+                numero_vaga = vagas_disponiveis.pop(0)
+                print(f"Apartamento será alocado na vaga {numero_vaga}.")
+            print("Escolha uma torre:")
+            for idx, torre in enumerate(torres, start=1):
+                print(f"{idx}. {torre.nome} - {torre.endereco}")
+            torre_opcao = int(input("Número da torre: "))
+            torre = torres[torre_opcao - 1]
+            id_apartamento = gerar_proximo_id()
+            apartamento = Apartamento(id_apartamento, numero_apartamento, numero_vaga, torre)
+
+            if numero_vaga is not None:
+                lista_apartamentos.adicionar_apartamento(apartamento)
+            else:
+                fila_de_espera.adicionar_apartamento(apartamento)
+
+        elif opcao == 2:
+            numero_vaga = int(input("Número da vaga a ser liberada: "))
+            vaga_liberada = lista_apartamentos.liberar_vaga(numero_vaga, fila_de_espera)
+            if vaga_liberada is not None:
+                novo_apartamento = fila_de_espera.retirar_apartamento(vaga_liberada)
+                if novo_apartamento is not None:
+                    lista_apartamentos.adicionar_apartamento(novo_apartamento)
+                vagas_disponiveis.append(vaga_liberada)
+                vagas_disponiveis.sort()
+
+        elif opcao == 3:
+            lista_apartamentos.imprimir_lista()
+
+        elif opcao == 4:
+            fila_de_espera.imprimir_fila()
+
+        elif opcao == 5:
+            print("Saindo...")
+            break
+
+        else:
+            print("Opção inválida. Tente novamente.")
+
 if __name__ == "__main__":
     menu()
